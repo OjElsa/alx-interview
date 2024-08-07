0e30dc1184732308940e9f48f06c73d3bec72a45
OjElsa <elsahojwang06@gmail.com>
2024-08-07 11:37:45 +0300

    Rotate Matrix 360 degrees

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,10 @@
+def rotate_2d_matrix(matrix):
+    # Transpose the matrix
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
